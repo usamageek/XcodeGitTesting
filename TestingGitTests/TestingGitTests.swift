75b13b5f17e45bf2b1d822bbb4d279547405b8f9
Muhammad Usama Mazhar <Anon@muhammads-mac.local>
2016-08-19 12:28:08 +0500

    Initial Commit

---
+++ TestingGitTests/TestingGitTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestingGitTests.swift
+//  TestingGitTests
+//
+//  Created by Muhammad Usama Mazhar on 19/08/2016.
+//  Copyright © 2016 Muhammad Usama Mazhar. All rights reserved.
+//
+
+import XCTest
+@testable import TestingGit
+
+class TestingGitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
